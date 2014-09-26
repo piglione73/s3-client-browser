f259c40076757d63dde79745573d38f3e7058d86
piglione73 <roberto.rossi@paviasystem.com>
2014-09-26 19:24:18 +0200

    Development

---
+++ js/s3-client-browser.js
@@ -0,0 +1,44 @@
+﻿var SCB = (function () {
+
+    function init(w) {
+        //Hide loading screen
+        $(".Loading").fadeOut();
+
+        getConnectionParameters();
+    }
+
+    function getConnectionParameters(callback) {
+        //Get connection parameters from localStorage. If not present, then ask the user
+        var params = localStorage["ConnectionParameters"];
+        alert("Params = " + params);
+    }
+
+    //Exports
+    return {
+        init: init
+    };
+})();
+
+
+/*
+// See the Configuring section to configure credentials in the SDK
+AWS.config = new AWS.Config({
+accessKeyId: 'AKID', secretAccessKey: 'SECRET', region: 'eu-west-1'
+});
+
+var bucket = new AWS.S3({ params: { Bucket: 'BUCKET'} });
+//bucket.makeUnauthenticatedRequest('listObjects', {Bucket: 'BUCKET'}, function (err, data) {
+bucket.listObjects(function (err, data) {
+if (err) {
+document.getElementById('status').innerHTML =
+'Could not load objects from S3: ' + err;
+} else {
+document.getElementById('status').innerHTML =
+'Loaded ' + data.Contents.length + ' items from S3';
+for (var i = 0; i < data.Contents.length; i++) {
+document.getElementById('objects').innerHTML +=
+'<li>' + data.Contents[i].Key + '</li>';
+}
+}
+});
+*/
