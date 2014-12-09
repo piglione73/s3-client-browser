8193d8705985f4767812493a76070effd773842f
piglione73 <roberto.rossi@paviasystem.com>
2014-12-09 20:06:18 +0100

    Test

--- java/ScaleAwsImages/src/Test.java
+++ java/ScaleAwsImages/src/Test.java
@@ -1,8 +1,12 @@
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+
 
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		AmazonS3Client s3 = new AmazonS3Client();
+		s3.listObjects(new ListObjectsRequest().withBucketName(""));
 	}
 
 }
